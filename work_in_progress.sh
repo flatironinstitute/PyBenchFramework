bfaf9e2875ac6d3cf5e995ed2232138d030cea9f
shockacone <skrit@flatironinstitute.org>
2024-05-23 20:12:07 -0400

    Updating the Slurm submit script to reflect current iteration

--- work_in_progress.sh
+++ work_in_progress.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
-#SBATCH -o PyBenchFramework-%j.log
-#SBATCH -e PyBenchFramework-%j.err
+#SBATCH -o pi-%j.log
+#SBATCH -e pi-%j.err
 #SBATCH -p scc
 #SBATCH --ntasks-per-node=20
-#SBATCH --nodes=14
+#SBATCH --nodes=10
 #SBATCH --reservation=ceph_test
-#SBATCH --mem=1G
+#SBATCH --time=40:00:00
 
-root_dir="/path/to/root/dir/"
+root_dir="/mnt/home/skrit/Documents/benchmark_handler/"
 
 filename="${root_dir}/host_files/${SLURM_JOB_ID}_hosts.file"
 
@@ -19,9 +19,53 @@ pdsh -w $SLURM_JOB_NODELIST "module load modules/2.3-beta1 modules/2.3-beta2 mod
 sleep 15
 
 if [[ -f ${filename} ]]; then
-	#python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 64K --directory /mnt/cephtest/test-rep3/skrit/fio/tmp/ --io-type randread --platform-type triple_replicated
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 4K --directory /mnt/cephtestk/test-rep3/skrit/fio/tmp/ --io-type "randread" --platform-type triple_replicated --split-file "True"
+	
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 4K --directory /mnt/cephtestk/test-ec63/skrit/fio-test/tmp/ --io-type "randread" --platform-type EC63
 
-	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 1M --directory /mnt/cephtest/test-rep3/skrit/fio/tmp/ --io-type randread --platform-type triple_replicated
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 64K --directory /mnt/cephtestk/test-rep3/skrit/fio/tmp/ --io-type "randread" --platform-type triple_replicated 
+
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 64K --directory /mnt/cephtestk/test-ec63/skrit/fio-test/tmp/ --io-type "randread" --platform-type EC63 
+
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 4M --directory /mnt/cephtestk/test-rep3/skrit/fio/tmp/ --io-type "randread" --platform-type triple_replicated
+	
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 4M --directory /mnt/cephtestk/test-ec63/skrit/fio-test/tmp/ --io-type "randread" --platform-type EC63
+	
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 4K --directory /mnt/cephtestk/test-rep3/skrit/fio/tmp/ --io-type "randwrite" --platform-type triple_replicated
+	
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 4K --directory /mnt/cephtestk/test-ec63/skrit/fio-test/tmp/ --io-type "randwrite" --platform-type EC63
+
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 64K --directory /mnt/cephtestk/test-rep3/skrit/fio/tmp/ --io-type "randwrite" --platform-type triple_replicated
+
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 64K --directory /mnt/cephtestk/test-ec63/skrit/fio-test/tmp/ --io-type "randwrite" --platform-type EC63 
+
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 4M --directory /mnt/cephtestk/test-rep3/skrit/fio/tmp/ --io-type "randwrite" --platform-type triple_replicated
+	
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 4M --directory /mnt/cephtestk/test-ec63/skrit/fio-test/tmp/ --io-type "randwrite" --platform-type EC63
+
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 4M --directory /mnt/cephtestk/test-rep3/skrit/fio/tmp/ --io-type "read" --platform-type triple_replicated 
+
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 4M --directory /mnt/cephtestk/test-ec63/skrit/fio-test/tmp/ --io-type "read" --platform-type EC63 
+
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 4M --directory /mnt/cephtestk/test-rep3/skrit/fio/tmp/ --io-type "write" --platform-type triple_replicated
+
+	#if [[ -f $filename ]]; then rm $filename; fi
+	python python_runs/multi_node.py --slurm-job-number ${SLURM_JOB_ID} --block-size 4M --directory /mnt/cephtestk/test-ec63/skrit/fio-test/tmp/ --io-type "write" --platform-type EC63
+
 else
 	echo "File ${filename} creation failed... Exiting."
 fi
