ce8c3fd682471cad431c598a325c516a157160d6
shockacone <skrit@flatironinstitute.org>
2024-06-06 18:30:30 -0400

    changed script to use srun instead of pdsh

--- submit_scripts/work_in_progress.sh
+++ submit_scripts/work_in_progress.sh
@@ -14,9 +14,27 @@ filename="${root_dir}/host_files/${SLURM_JOB_ID}_hosts.file"
 if [[ -f $filename ]]; then rm $filename; fi
 
 
-pdsh -w $SLURM_JOB_NODELIST "source ${root_dir}/env_start; IP=\$(ip a | grep 147 | awk '{print \$2}' | cut -d / -f 1 | head -n1); echo \"\${IP},5201\" >> ${filename}; fio --server=\"\${IP},5201\"" &
+# Use srun to run the commands on all allocated nodes
+srun --nodes=$SLURM_JOB_NUM_NODES --ntasks-per-node=1 bash -c "
+    # Debugging output
+    echo \"Root directory: ${root_dir}\"
+    echo \"Filename: ${filename}\"
+    
+    # Source the environment setup script
+    source ${root_dir}/env_start || { echo \"Failed to source ${root_dir}/env_start\"; exit 1; }
 
-sleep 15
+    # Retrieve the IP address
+    IP=\$(ip -o -4 addr list | awk '{print \$2, \$4}' | grep eno1 | awk '{ print \$NF }' | cut -d / -f 1)
+    echo \"IP Address: \${IP}\"
+    
+    # Append the IP and port to the specified file
+    echo \"\${IP},5201\" >> ${filename} || { echo \"Failed to write to ${filename}\"; exit 1; }
+
+    # Start the fio server
+    fio --server=\"\${IP},5201\"
+" &
+
+sleep 20 
 
 if [[ -f ${filename} ]]; then
 	#starting run to lay out all files
