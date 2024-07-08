f72c986a258e404571f7003048080da82cd354c3
shockacone <skrit@flatironinstitute.org>
2024-07-08 16:18:41 -0400

    Cleaning up job script

--- submit_scripts/client_second_test.sh
+++ submit_scripts/client_second_test.sh
@@ -14,26 +14,12 @@ block_size="1M"
 
 first_node=$(echo $SLURM_JOB_NODELIST | cut -d '-' -f 1 | awk -F '[' '{ print $1$2 }')
 
-start_time=$(date +%s)
-
-echo "${first_node}"
-
 python python_runs/prep_work.py --benchmark "fio-serverless" --slurm-job-number ${SLURM_JOB_ID} --block-size 1M --config python_runs/fsync_test_ceph_test.yml --first-node ${first_node}
 
 sleep 10
 
-srun --nodes=$SLURM_JOB_NUM_NODES python python_runs/run.py --benchmark "fio-serverless" --slurm-job-number ${SLURM_JOB_ID} --block-size 1M --config python_runs/fsync_test_ceph_test.yml --first-node ${first_node}
+srun --nodes=10 python python_runs/run.py --benchmark "fio-serverless" --slurm-job-number ${SLURM_JOB_ID} --block-size 1M --config python_runs/fsync_test_ceph_test.yml --first-node ${first_node} --node-count 10
 
-end_time=$(date +%s)
+srun --nodes=5 python python_runs/run.py --benchmark "fio-serverless" --slurm-job-number ${SLURM_JOB_ID} --block-size 1M --config python_runs/fsync_test_ceph_test.yml --first-node ${first_node} --node-count 5
 
-# Run the Python multi-node script
-echo "Start FIO jobs"
-echo "Stop FIO jobs"
-
-# Print the start and end times
-echo "Start time: ${start_time}"
-echo "End time: ${end_time}"
-
-#wait $srun_pid
-
-echo "srun command has finished."
+srun --nodes=1 python python_runs/run.py --benchmark "fio-serverless" --slurm-job-number ${SLURM_JOB_ID} --block-size 1M --config python_runs/fsync_test_ceph_test.yml --first-node ${first_node} --node-count 1
