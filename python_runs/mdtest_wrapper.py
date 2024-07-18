c95ea5e6d45b130c236d08cdfa5fafd858525f5e
shockacone <skrit@flatironinstitute.org>
2024-07-18 15:44:09 -0400

    Updating mdtest wrapper to allow for iteration over number of ranks and files per rank

--- python_runs/mdtest_wrapper.py
+++ python_runs/mdtest_wrapper.py
@@ -16,33 +16,39 @@ def wrap_mdtest(args, PyBench_root_dir):
 
     current_dir = os.getcwd()
 
-    mdtest_obj = handler_class.mdtestTool()
+    mdtest_obj_dict = {}
+    #handler_class.mdtestTool()
 
     log_dir = f"{PyBench_root_dir}/results/{args['io_type']}/{args['platform_type']}/{job_number}"
     command_log_dir = f"{log_dir}/commands"
     
     hostname = socket.gethostname()
 
-    mpi_ranks = args['mpi_ranks']
-    files_per_rank = args['files_per_rank']
+    mpi_ranks = benchmark_tools.split_arg_sequence(args['mpi_ranks'], "--mpi-ranks")
+    files_per_rank_list = benchmark_tools.split_arg_sequence(args['files_per_rank'], "--files-per-rank")
     test_repetition = args['test_repetition']
     directory = args['directory']
     offset = args['offset']
 
-    mdtest_obj.setup_command(config_file=f"{PyBench_root_dir}/{args['config']}", mpi_ranks=f"{mpi_ranks}", files_per_rank=f"{files_per_rank}", test_repetition=f"{test_repetition}", directory=f"{directory}", offset=f"{offset}", output_file=f"{log_dir}/mdtest_output")
-        
-    with open(f"{command_log_dir}/mdtest_command", 'a') as file:
-        file.write(f"The following is the mdtest command")
-        tmp_cmd_string = ""
-        for cmd_el in mdtest_obj.command:
-            tmp_cmd_string += f" {cmd_el}"
-        file.write(tmp_cmd_string)
-    
-    start_time = time.time()
-    mdtest_obj.run()
-    end_time = time.time()
+    for rank in mpi_ranks:
+        for files_per_rank in files_per_rank_list:
 
-    elapsed_time = end_time - start_time
-    print(f"mdtest job is finished. [s-{start_time}], [e-{end_time}]")
-    
-    sys.stdout.flush()
+            mdtest_obj_dict[f"{rank}_ranks_{files_per_rank}_files_per_rank"] = handler_class.mdtestTool()
+
+            mdtest_obj_dict[f"{rank}_ranks_{files_per_rank}_files_per_rank"].setup_command(config_file=f"{PyBench_root_dir}/{args['config']}", mpi_ranks=f"{rank}", files_per_rank=f"{files_per_rank}", test_repetition=f"{test_repetition}", directory=f"{directory}", offset=f"{offset}", output_file=f"{log_dir}/mdtest_output_{rank}_ranks_{files_per_rank}_files_per_rank")
+        
+            with open(f"{command_log_dir}/mdtest_command_{rank}_ranks_{files_per_rank}_files_per_rank", 'a') as file:
+                file.write(f"The following is the mdtest command")
+                tmp_cmd_string = ""
+                for cmd_el in mdtest_obj_dict[f"{rank}_ranks_{files_per_rank}_files_per_rank"].command:
+                    tmp_cmd_string += f" {cmd_el}"
+                file.write(tmp_cmd_string)
+            
+            start_time = time.time()
+            mdtest_obj_dict[f"{rank}_ranks_{files_per_rank}_files_per_rank"].run()
+            end_time = time.time()
+            
+            elapsed_time = end_time - start_time
+            print(f"mdtest job {rank}_ranks_{files_per_rank}_files_per_rank is finished. [s-{start_time}], [e-{end_time}]")
+            
+            sys.stdout.flush()
